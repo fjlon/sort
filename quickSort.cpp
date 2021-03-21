e5bba72eebe0a66d56cf0dec709312e54ee4b13d
fjlon <81084756+fjlon@users.noreply.github.com>
2021-03-21 20:18:20 +0800

    Create quickSort.cpp
    快速排序算法实现，采用前后扫描进行分割。

---
+++ quickSort.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int patiton(int* value,int low,int high)
+{
+    if(!value||low>=high)
+        return -1;
+    int vCmp = value[low];
+    while(low<high)
+    {
+        while(low<high&&value[high]>=vCmp){
+            high--;
+        }
+        value[low]=value[high];
+        while(low<high&&value[low]<=vCmp){
+            low++;
+        }
+        value[high]=value[low];
+    }
+    value[low]=vCmp;
+    return low;
+}
+
+int qsort(int* value,int low,int high)
+{
+    if(!value||low>=high)
+        return -1;
+    int position = patiton(value,low,high);
+    qsort(value,low,position-1);
+    qsort(value,position+1,high);
+    return 0;
+}
